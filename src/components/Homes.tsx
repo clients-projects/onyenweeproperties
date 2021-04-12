37dcd36d60800d90e221a85cef980a71929877a8
Igboanugwo Collins <munisco12@gmail.com>
2021-04-12 15:55:07 +0100

    working on files

--- src/components/Homes.tsx
+++ src/components/Homes.tsx
@@ -4,7 +4,7 @@ const Homes = () => {
         <div className="home">
             <img src="img/house-1.jpeg" alt="House 1" className="home__img"/>
             <svg className="home__like">
-                <use xlinkHref="img/sprite.svg#icon-heart-full"></use>
+                {/* <use xlinkHref="img/sprite.svg#icon-heart-full"></use> */}
             </svg>
             <h5 className="home__name">Beautiful Family Home</h5>
             <div className="home__location">
@@ -16,21 +16,21 @@ const Homes = () => {
 
             <div className="home__rooms">
                 <svg>
-                    <use xlinkHref="img/sprite.svg#icon-profile-male"></use>
+                    {/* <use xlinkHref="img/sprite.svg#icon-profile-male"></use> */}
                 </svg>
                 <p>5 rooms</p>
             </div>
 
             <div className="home__area">
                 <svg>
-                    <use xlinkHref="img/sprite.svg#icon-expand"></use>
+                    {/* <use xlinkHref="img/sprite.svg#icon-expand"></use> */}
                 </svg>
                 <p>325 m<sup>2</sup></p>
             </div>
 
             <div className="home__price">
                 <svg>
-                    <use xlinkHref="img/sprite.svg#icon-key"></use>
+                    {/* <use xlinkHref="img/sprite.svg#icon-key"></use> */}
                 </svg>
                 <p>$1,200,000</p>
             </div>
@@ -40,33 +40,33 @@ const Homes = () => {
         <div className="home">
             <img src="img/house-2.jpeg" alt="House 1" className="home__img"/>
             <svg className="home__like">
-                <use xlinkHref="img/sprite.svg#icon-heart-full"></use>
+                {/* <use xlinkHref="img/sprite.svg#icon-heart-full"></use> */}
             </svg>
             <h5 className="home__name">Modern Glass Villa</h5>
             <div className="home__location">
                 <svg>
-                    <use xlinkHref="img/sprite.svg#icon-map-pin"></use>
+                    {/* <use xlinkHref="img/sprite.svg#icon-map-pin"></use> */}
                 </svg>
                 <p>Canada</p>
             </div>
 
             <div className="home__rooms">
                 <svg>
-                    <use xlinkHref="img/sprite.svg#icon-profile-male"></use>
+                    {/* <use xlinkHref="img/sprite.svg#icon-profile-male"></use> */}
                 </svg>
                 <p>6 rooms</p>
             </div>
 
             <div className="home__area">
                 <svg>
-                    <use xlinkHref="img/sprite.svg#icon-expand"></use>
+                    {/* <use xlinkHref="img/sprite.svg#icon-expand"></use> */}
                 </svg>
                 <p>450 m<sup>2</sup></p>
             </div>
 
             <div className="home__price">
                 <svg>
-                    <use xlinkHref="img/sprite.svg#icon-key"></use>
+                    {/* <use xlinkHref="img/sprite.svg#icon-key"></use> */}
                 </svg>
                 <p>$2,750,000</p>
             </div>
@@ -76,33 +76,33 @@ const Homes = () => {
         <div className="home">
             <img src="img/house-3.jpeg" alt="House 3" className="home__img"/>
             <svg className="home__like">
-                <use xlinkHref="img/sprite.svg#icon-heart-full"></use>
+                {/* <use xlinkHref="img/sprite.svg#icon-heart-full"></use> */}
             </svg>
             <h5 className="home__name">Cozy Country House</h5>
             <div className="home__location">
                 <svg>
-                    <use xlinkHref="img/sprite.svg#icon-map-pin"></use>
+                    {/* <use xlinkHref="img/sprite.svg#icon-map-pin"></use> */}
                 </svg>
                 <p>UK</p>
             </div>
 
             <div className="home__rooms">
                 <svg>
-                    <use xlinkHref="img/sprite.svg#icon-profile-male"></use>
+                    {/* <use xlinkHref="img/sprite.svg#icon-profile-male"></use> */}
                 </svg>
                 <p>4 rooms</p>
             </div>
 
             <div className="home__area">
                 <svg>
-                    <use xlinkHref="img/sprite.svg#icon-expand"></use>
+                    {/* <use xlinkHref="img/sprite.svg#icon-expand"></use> */}
                 </svg>
                 <p>250 m<sup>2</sup></p>
             </div>
 
             <div className="home__price">
                 <svg>
-                    <use xlinkHref="img/sprite.svg#icon-key"></use>
+                    {/* <use xlinkHref="img/sprite.svg#icon-key"></use> */}
                 </svg>
                 <p>$850,000</p>
             </div>
@@ -112,7 +112,7 @@ const Homes = () => {
         <div className="home">
             <img src="img/house-1.jpeg" alt="House 1" className="home__img"/>
             <svg className="home__like">
-                <use xlinkHref="img/sprite.svg#icon-heart-full"></use>
+                {/* <use xlinkHref="img/sprite.svg#icon-heart-full"></use> */}
             </svg>
             <h5 className="home__name">Large Country Villa</h5>
             <div className="home__location">
