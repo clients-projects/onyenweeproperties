c4fe687a10af93ba68ff8ed52c3c50b120f6f165
Igboanugwo Collins <munisco12@gmail.com>
2021-04-12 15:27:30 +0100

    working on files

---
+++ src/components/Footer.tsx
@@ -0,0 +1,45 @@
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <ul className='nav'>
+                <li className='nav__item'>
+                    <a href='#' className='nav__link'>
+                        Find your dream home
+                    </a>
+                </li>
+                <li className='nav__item'>
+                    <a href='#' className='nav__link'>
+                        Request a proposal
+                    </a>
+                </li>
+                <li className='nav__item'>
+                    <a href='#' className='nav__link'>
+                        Download your home planner
+                    </a>
+                </li>
+                <li className='nav__item'>
+                    <a href='#' className='nav__link'>
+                        Contact us
+                    </a>
+                </li>
+                <li className='nav__item'>
+                    <a href='#' className='nav__link'>
+                        Submit your property
+                    </a>
+                </li>
+                <li className='nav__item'>
+                    <a href='#' className='nav__link'>
+                        Come work with us
+                    </a>
+                </li>
+            </ul>
+            <p className='copyright'>
+                &copy;Copyright 2018 by Jonas Schmedtmann. Approved for use by
+                Jonas Schmedtmann for use in personal projects but not for
+                tutorials or video courses based on this project.
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
