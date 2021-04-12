af86daec957a535b4c606116c8fbff5f2e20f5ae
Igboanugwo Collins <munisco12@gmail.com>
2021-04-12 15:49:58 +0100

    working on files

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -7,6 +7,11 @@ import Gal6 from '../sass/img/gal-6.jpeg'
 import Gal7 from '../sass/img/gal-7.jpeg'
 import Gal8 from '../sass/img/gal-8.jpeg'
 import Gal9 from '../sass/img/gal-9.jpeg'
+import Gal10 from '../sass/img/gal-10.jpeg'
+import Gal11 from '../sass/img/gal-11.jpeg'
+import Gal12 from '../sass/img/gal-12.jpeg'
+import Gal13 from '../sass/img/gal-13.jpeg'
+import Gal14 from '../sass/img/gal-14.jpeg'
 
 const Gallery = () => {
     return (
@@ -15,49 +20,49 @@ const Gallery = () => {
             <img src={Gal1} alt="1" className="gallery__img"/>
         </figure>
         <figure className="gallery_item gallery__item--2">
-            <img src="img/gal-2.jpeg" alt="2" className="gallery__img"/>
+            <img src={Gal2} alt="2" className="gallery__img"/>
         </figure>
         <figure className="gallery_item gallery__item--3">
-            <img src="img/gal-3.jpeg" alt="3" className="gallery__img"/>
+            <img src={Gal3} alt="3" className="gallery__img"/>
         </figure>
         <figure className="gallery_item gallery__item--4">
-            <img src="img/gal-4.jpeg" alt="4" className="gallery__img"/>
+            <img src={Gal4} alt="4" className="gallery__img"/>
         </figure>
         <figure className="gallery_item gallery__item--5">
-            <img src="img/gal-5.jpeg" alt="5" className="gallery__img"/>
+            <img src={Gal5} alt="5" className="gallery__img"/>
         </figure>
         <figure className="gallery_item gallery__item--6">
-            <img src="img/gal-6.jpeg" alt="6" className="gallery__img"/>
+            <img src={Gal6} alt="6" className="gallery__img"/>
         </figure>
         <figure className="gallery_item gallery__item--7">
-            <img src="img/gal-7.jpeg" alt="7" className="gallery__img"/>
+            <img src={Gal7} alt="7" className="gallery__img"/>
         </figure>
         <figure className="gallery_item gallery__item--8">
-            <img src="img/gal-8.jpeg" alt="8" className="gallery__img"/>
+            <img src={Gal8} alt="8" className="gallery__img"/>
         </figure>
 
         <figure className="gallery_item gallery__item--9">
-            <img src="img/gal-9.jpeg" alt="9" className="gallery__img"/>
+            <img src={Gal9} alt="9" className="gallery__img"/>
         </figure>
 
         <figure className="gallery_item gallery__item--10">
-            <img src="img/gal-10.jpeg" alt="10" className="gallery__img"/>
+            <img src={Gal10} alt="10" className="gallery__img"/>
         </figure>
 
         <figure className="gallery_item gallery__item--11">
-            <img src="img/gal-11.jpeg" alt="11" className="gallery__img"/>
+            <img src={Gal11} alt="11" className="gallery__img"/>
         </figure>
 
         <figure className="gallery_item gallery__item--12">
-            <img src="img/gal-12.jpeg" alt="12" className="gallery__img"/>
+            <img src={Gal12} alt="12" className="gallery__img"/>
         </figure>
 
         <figure className="gallery_item gallery__item--13">
-            <img src="img/gal-13.jpeg" alt="13" className="gallery__img"/>
+            <img src={Gal13} alt="13" className="gallery__img"/>
         </figure>
 
         <figure className="gallery_item gallery__item--14">
-            <img src="img/gal-14.jpeg" alt="14" className="gallery__img"/>
+            <img src={Gal14} alt="14" className="gallery__img"/>
         </figure>
     </section>
     )
