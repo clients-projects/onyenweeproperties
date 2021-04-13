8f00dd73c53eb2b8892755f1a28a478293079467
Igboanugwo Collins <munisco12@gmail.com>
2021-04-13 11:43:07 +0100

    Added social media handles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,35 @@
-import LogoBBC from '../sass/img/logo-bbc.png'
-import LogoForbes from '../sass/img/logo-forbes.png'
-import LogoTechcrunch from '../sass/img/logo-techcrunch.png'
-import LogoBi from '../sass/img/logo-bi.png'
 import Logo from '../sass/img/logo.png'
 import { FaFacebook, FaInstagramSquare } from 'react-icons/fa'
 import { AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-         <header className="header">
-        <img src={Logo} alt="Nexter logo" className="header__logo"/>
-        <h3 className="heading-3">Your own home:</h3>
-        <h1 className="heading-1">The ultimate personal freedom</h1>
-        <button className="btn header__btn">View our properties</button>
-        <div className="header__followon-text">Follow us on</div>
-        <div className="header__followon-logos">
-            <FaFacebook/>
-            <FaInstagramSquare/>
-            <AiFillTwitterCircle/>
-            <AiFillLinkedin/>
-          
-        </div>
-    </header>
+        <header className='header'>
+            <img src={Logo} alt='Nexter logo' className='header__logo' />
+            <h3 className='heading-3'>Your own home:</h3>
+            <h1 className='heading-1'>The ultimate personal freedom</h1>
+            <button className='btn header__btn'>View our properties</button>
+            <div className='header__followon-text'>Follow us on</div>
+            <div className='header__followon-logos'>
+                <Link to='/' className='header__followon-link'>
+                    {' '}
+                    <FaFacebook/>
+                </Link>
+                <Link to='/' className='header__followon-link'>
+                    {' '}
+                    <FaInstagramSquare />
+                </Link>
+                <Link to='/' className='header__followon-link'>
+                    {' '}
+                    <AiFillTwitterCircle />
+                </Link>
+                <Link to='/' className='header__followon-link'>
+                    {' '}
+                    <AiFillLinkedin />
+                </Link>
+            </div>
+        </header>
     )
 }
 
