5b05deecb065c89dfe59cdd723106ffd1b363f34
Igboanugwo Collins <munisco12@gmail.com>
2021-04-13 12:00:50 +0100

    Working on realtors

--- src/components/Realtors.tsx
+++ src/components/Realtors.tsx
@@ -1,12 +1,13 @@
-import Realtor2 from '../sass/img/realtor-2.jpeg'
-import Realtor3 from '../sass/img/realtor-3.jpeg'
-import Realtor1 from '../sass/img/gal-2.jpeg'
+
+import Realtor1 from '../sass/img/gal-1.jpeg'
+import Realtor2 from '../sass/img/gal-2.jpeg'
+import Realtor3 from '../sass/img/gal-3.jpeg'
 
 
 const Realtors = () => {
     return (
         <div className='realtors'>
-            <h3 className='heading-3'>Top Houses</h3>
+            <h3 className='heading-3'>Top Picks</h3>
             <div className='realtors__list'>
                 <img src={Realtor1} alt='Realtor 1' className='realtors__img' />
                 <div className='realtors__details'>
@@ -18,7 +19,7 @@ const Realtors = () => {
                 <img src={Realtor2} alt='Realtor 2' className='realtors__img' />
                 <div className='realtors__details'>
                     <h4 className='heading-4 heading-4--light'>
-                        Commericial Buildings
+                        Commercial Buildings
                     </h4>
                     <p className='realtors__sold'>212 houses sold</p>
                 </div>
