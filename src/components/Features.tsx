0ac3d97b7273937c68118666d866623494a6b9dd
Igboanugwo Collins <munisco12@gmail.com>
2021-04-14 05:23:32 +0100

    Adding icons to features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,3 +1,5 @@
+import {GrMapLocation} from 'react-icons/gr'
+
 const Features = () => {
     return (
         <section className="features">
@@ -31,36 +33,7 @@ const Features = () => {
                 quidem consequatur harum.
             </p>
         </div>
-        <div className="feature">
-            <svg className="feature__icon">
-                {/* <use xlinkHref="img/sprite.svg#icon-key"></use> */}
-            </svg>
-            <h4 className="heading-4 heading-4--dark">New home in one week</h4>
-            <p className="feature__text">
-                Vero ipsum sapiente molestias accusamus rerum.
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-        </div>
-        <div className="feature">
-            <svg className="feature__icon">
-                {/* <use xlinkHref="img/sprite.svg#icon-presentation"></use> */}
-            </svg>
-            <h4 className="heading-4 heading-4--dark">Top 1% realtors</h4>
-            <p className="feature__text">
-                Quidem consequatur harum, voluptatum mollitia quae.
-                Tenetur distinctio necessitatibus pariatur voluptatibus.
-            </p>
-        </div>
-        <div className="feature">
-            <svg className="feature__icon">
-                {/* <use xlinkHref="img/sprite.svg#icon-lock"></use> */}
-            </svg>
-            <h4 className="heading-4 heading-4--dark">Secure payments on XXX</h4>
-            <p className="feature__text">
-                Pariatur voluptatibus quidem consequatur harum,
-                voluptatum mollitia quae.
-            </p>
-        </div>
+
     </section>
     )
 }
