99c1b5170abe403a656cc3a1a8a771c9947daf23
Igboanugwo Collins <munisco12@gmail.com>
2021-04-15 19:25:27 +0100

    Added animated nav button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -48,10 +48,55 @@ const Sidebar = () => {
 
             <header className='header' id='header'>
                 <div className='header__toggle'>
-                    <i class='bx bx-menu' id='header-toggle'></i>
+                    <i className='bx bx-menu' id='header-toggle'></i>
                 </div>
-                <div className="header__img">
-                    <img src={Logo} alt="logo"/>
+                <div className='header__img'>
+                    <img src={Logo} alt='logo' />
+                </div>
+                <div className='1-navbar' id='nav-bar'>
+                    <nav className='nav'>
+                        <div>
+                            <Link to='/' className='nav__logo'>
+                                <i className='bx bx-layer nav__logo-icon'></i>
+                                <span className='nav__logo-name'>
+                                    Bedimcode
+                                </span>
+                            </Link>
+
+                            <div className='nav__list'>
+                                <Link to='/' className='nav__logo'>
+                                    <i className='bx bx-layer nav__logo-icon'></i>
+                                    <span className='nav__logo-name'>
+                                        Collins
+                                    </span>
+                                </Link>
+                                <Link to='/' className='nav__logo'>
+                                    <i className='bx bx-layer nav__logo-icon'></i>
+                                    <span className='nav__logo-name'>
+                                        Bedimcode
+                                    </span>
+                                </Link>
+                                <Link to='/' className='nav__logo'>
+                                    <i className='bx bx-layer nav__logo-icon'></i>
+                                    <span className='nav__logo-name'>
+                                        Bedimcode
+                                    </span>
+                                </Link>
+                                <Link to='/' className='nav__logo'>
+                                    <i className='bx bx-layer nav__logo-icon'></i>
+                                    <span className='nav__logo-name'>
+                                        Bedimcode
+                                    </span>
+                                </Link>
+                                <Link to='/' className='nav__logo'>
+                                    <i className='bx bx-layer nav__logo-icon'></i>
+                                    <span className='nav__logo-name'>
+                                        Bedimcode
+                                    </span>
+                                </Link>
+                            </div>
+                        </div>
+                    </nav>
                 </div>
             </header>
         </div>
