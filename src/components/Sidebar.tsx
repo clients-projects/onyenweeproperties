35cb61355626c7ac092e986332e3fff4823e5031
Igboanugwo Collins <munisco12@gmail.com>
2021-04-15 21:30:42 +0100

    Added animated nav button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,57 +3,15 @@ import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
     return (
-        <div className='sidebar'>
-            <img src={Logo} alt='logo' className='sidebar-logo' />
-
-            <label htmlFor='check'>
-                <input type='checkbox' id='check' />
-                <span></span>
-                <span></span>
-                <span></span>
-            </label>
-
-            <ul className='sidebar-nav'>
-                <li>
-                    <Link to='/' className='sidebar-nav__item'>
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link to='/' className='sidebar-nav__item'>
-                        Real Estate
-                    </Link>
-                </li>
-                <li>
-                    <Link to='/' className='sidebar-nav__item'>
-                        Property Management
-                    </Link>
-                </li>
-                <li>
-                    <Link to='/' className='sidebar-nav__item'>
-                        Construction
-                    </Link>
-                </li>
-                <li>
-                    <Link to='/' className='sidebar-nav__item'>
-                        Interior
-                    </Link>
-                </li>
-                <li>
-                    <Link to='/' className='sidebar-nav__item'>
-                        Contact us
-                    </Link>
-                </li>
-            </ul>
-
-            <header className='header' id='header'>
+        <>
+                    <header className='header' id='header'>
                 <div className='header__toggle'>
                     <i className='bx bx-menu' id='header-toggle'></i>
                 </div>
                 <div className='header__img'>
                     <img src={Logo} alt='logo' />
                 </div>
-                <div className='1-navbar' id='nav-bar'>
+                <div className='navbar1' id='nav-bar'>
                     <nav className='nav'>
                         <div>
                             <Link to='/' className='nav__logo'>
@@ -99,7 +57,52 @@ const Sidebar = () => {
                     </nav>
                 </div>
             </header>
+        <div className='sidebar'>
+            <img src={Logo} alt='logo' className='sidebar-logo' />
+
+            <label htmlFor='check'>
+                <input type='checkbox' id='check' />
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+
+            <ul className='sidebar-nav'>
+                <li>
+                    <Link to='/' className='sidebar-nav__item'>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/' className='sidebar-nav__item'>
+                        Real Estate
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/' className='sidebar-nav__item'>
+                        Property Management
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/' className='sidebar-nav__item'>
+                        Construction
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/' className='sidebar-nav__item'>
+                        Interior
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/' className='sidebar-nav__item'>
+                        Contact us
+                    </Link>
+                </li>
+            </ul>
+
+
         </div>
+        </>
     )
 }
 
