aeee6797269c150f0e4d0be5c314831331b8f122
Igboanugwo Collins <munisco12@gmail.com>
2021-04-16 22:58:01 +0100

    Building the backdrop

--- src/components/Backdrop.tsx
+++ src/components/Backdrop.tsx
@@ -1,22 +1,23 @@
 const Backdrop = (props: any) => {
     const handleBackdrop = () => {
-      const toggle = document.getElementById('sidebar-toggle'),
-          nav = document.getElementById('nav-bar'),
-          bodySelector = document.getElementById('bodyId'),
-          sidebar = document.getElementById('sidebar'),
-          header = document.getElementById('header-body')
+        const toggle = document.getElementById('sidebar-toggle'),
+            nav = document.getElementById('nav-bar'),
+            bodySelector = document.getElementById('bodyId'),
+            sidebar = document.getElementById('sidebar'),
+            header = document.getElementById('header-body')
 
-      if (toggle && nav && bodySelector && sidebar && header) {
-          toggle.addEventListener('click', () => {
-              nav.classList.remove('show')
-              toggle.classList.remove('bx-x')
-              bodySelector.classList.remove('bodyId')
-              sidebar.classList.remove('bodyId')
-              sidebar.classList.remove('toggleMenu')
-              sidebar.classList.remove('toggleColor')
+        if (toggle && nav && bodySelector && sidebar && header) {
+            toggle.addEventListener('click', () => {
+                nav.classList.remove('show')
+                toggle.classList.remove('bx-x')
+                bodySelector.classList.remove('bodyId')
+                sidebar.classList.remove('bodyId')
+                sidebar.classList.remove('toggleMenu')
+                sidebar.classList.remove('toggleColor')
+            })
 
-          })
-      }
+            console.log('toggle backdrop')
+        }
     }
 
     return (
