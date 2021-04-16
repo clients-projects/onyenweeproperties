156f6d184d80a6637ffc7fd883a8210f7e807b21
Igboanugwo Collins <munisco12@gmail.com>
2021-04-16 22:55:30 +0100

    Building the backdrop

--- src/components/Backdrop.tsx
+++ src/components/Backdrop.tsx
@@ -1,7 +1,22 @@
 const Backdrop = (props: any) => {
     const handleBackdrop = () => {
-        const nav = document.getElementById('nav-bar')
-        nav!.classList.toggle('show')
+      const toggle = document.getElementById('sidebar-toggle'),
+          nav = document.getElementById('nav-bar'),
+          bodySelector = document.getElementById('bodyId'),
+          sidebar = document.getElementById('sidebar'),
+          header = document.getElementById('header-body')
+
+      if (toggle && nav && bodySelector && sidebar && header) {
+          toggle.addEventListener('click', () => {
+              nav.classList.toggle('show')
+              toggle.classList.toggle('bx-x')
+              bodySelector.classList.toggle('bodyId')
+              sidebar.classList.toggle('bodyId')
+              sidebar.classList.toggle('toggleMenu')
+              sidebar.classList.toggle('toggleColor')
+
+          })
+      }
     }
 
     return (
