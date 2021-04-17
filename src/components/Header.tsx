417a1a54afd2d5772e39430d3f339027942d690b
Igboanugwo Collins <munisco12@gmail.com>
2021-04-17 21:58:19 +0100

    Added animated nav button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,52 +3,59 @@ import { Link } from 'react-router-dom'
 
 import { useEffect } from 'react'
 import Backdrop from './toggleNav/Backdrop'
-import { showNavBar } from './toggleNav/ShowNavBar'
 
 const Header = () => {
-    // useEffect(() => {
-    //     showNavBar(
-    //         'sidebar-toggle',
-    //         'nav-bar',
-    //         'bodyId',
-    //         'sidebar',
-    //         'header-body',
-    //         'header-logo',
-    //         'backdrop'
-    //     )
-    // }, [])
+    useEffect(() => {
+        showNavBar(
+            'sidebar-toggle',
+            'nav-bar',
+            'bodyId',
+            'sidebar',
+            'header-body',
+            'header-logo',
+            'backdrop'
+        )
+    }, [])
+
+    const showNavBar = (
+        toggleId: string,
+        navId: string,
+        bodyId: string,
+        sidebarId: string,
+        headerId: string,
+        logoId: string,
+        backdropId: string
+    ) => {
+        const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId),
+            bodySelector = document.getElementById(bodyId),
+            sidebar = document.getElementById(sidebarId),
+            header = document.getElementById(headerId),
+            logo = document.getElementById(logoId),
+            getBackdrop = document.getElementById(backdropId)
+
+        if (toggle && nav && bodySelector && sidebar && header) {
+            toggle.addEventListener('click', () => {
+                nav.classList.toggle('show')
+                toggle.classList.toggle('bx-x')
+                bodySelector.classList.toggle('bodyId')
+                sidebar.classList.toggle('bodyId')
+                sidebar.classList.toggle('toggleMenu')
+                sidebar.classList.toggle('toggleColor')
+                logo!.classList.toggle('toggleLogo')
+                getBackdrop!.classList.toggle('toggleBackdrop')
+            })
+        }
+    }
 
     return (
         <>
-            <section
-                className='sidebar'
-                id='sidebar'
-                onClick={() =>
-                    showNavBar(
-                        'sidebar-toggle',
-                        'nav-bar',
-                        'bodyId',
-                        'sidebar',
-                        'header-body',
-                        'header-logo',
-                        'backdrop'
-                    )
-                }
-            >
+            <section className='sidebar' id='sidebar'>
                 <div className='sidebar__toggle'>
                     <i className='bx bx-menu' id='sidebar-toggle'></i>
                 </div>
 
-                <Backdrop onClick={() =>
-                    showNavBar(
-                        'sidebar-toggle',
-                        'nav-bar',
-                        'bodyId',
-                        'sidebar',
-                        'header-body',
-                        'header-logo',
-                        'backdrop'
-                    )}>
+                <Backdrop>
                     <div className='navbar1' id='nav-bar'>
                         <nav className='nav'>
                             <div>
--- src/components/toggleNav/Backdrop.tsx
+++ src/components/toggleNav/Backdrop.tsx
@@ -1,19 +1,6 @@
-import { useEffect } from "react"
-import { showNavBar } from "./ShowNavBar"
 
 const Backdrop = (props: any) => {
-   useEffect(() => {
-       showNavBar(
-           'sidebar-toggle',
-           'nav-bar',
-           'bodyId',
-           'sidebar',
-           'header-body',
-           'header-logo',
-           'backdrop'
-       )
-   }, [])
-
+   
     return (
         <div
             className='backdrop'
