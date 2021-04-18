0c0f60a82c5852d295804d3cc174cadf7444a119
Igboanugwo Collins <munisco12@gmail.com>
2021-04-18 07:14:32 +0100

    Added other pages

--- src/Containers/App.tsx
+++ src/Containers/App.tsx
@@ -15,22 +15,6 @@ import { LastLocationType } from 'react-router-last-location'
 function App() {
    const location: LastLocationType = useLocation()
 
-
-
-    // const ref = useRef()
-    // useEffect(() => {
-    //     const token = localStorage.getItem('token')
-    //     const userId = localStorage.getItem('userId')
-    //     if (!ref.current) {
-    //         if (token) {
-    //             props.onCheckState(token, userId)
-    //         } else {
-    //             props.onInitActivities()
-    //         }
-    //     }
-    //     ref.current = true
-    // }, [props])
-
   
     let AuthGuard = (
         <Switch>
@@ -40,57 +24,17 @@ function App() {
                 component={Home}
                 render={Home}
             />
-            {/* <Route
-                path='/Auth/login'
-                render={(props) => <LoginPage {...props} />}
-            />
-            <Route
-                path='/Auth/signup'
-                render={(props) => <SignupPage {...props} />}
-            />
+        
 
             <Route path='/about-us' component={AboutUs} />
             <Route path='/forgot-password' component={ForgotPassword} />
             <Route path='/contact-us' component={ContactUs} />
             <Route path='/faq' component={Faq} />
-            <Route path='/terms' component={Terms} /> */}
+            <Route path='/terms' component={Terms} />
             <Redirect to='/' />
         </Switch>
     )
-    // if (props.auth) {
-    //     let admintoShow = (
-    //         <Route
-    //             path='/admin'
-    //             render={(props) => <AdminLayout {...props} />}
-    //         />
-    //     )
-
-    //     if (props.siteOwner) {
-    //         admintoShow = (
-    //             <Route
-    //                 path='/admin'
-    //                 render={(props) => <SiteOwnerAdmin {...props} />}
-    //             />
-    //         )
-    //     }
-
-    //     AuthGuard = (
-    //         <Switch>
-    //             <Route path='/' exact component={Home} />
-    //             <Route
-    //                 path='/Auth/login'
-    //                 render={(props) => <AsyncLogin {...props} />}
-    //             />
-    //             <Route path='/about-us' component={AsyncAboutUs} />
-    //             <Route path='/contact-us' component={ContactUs} />
-    //             <Route path='/faq' component={Faq} />
-    //             <Route path='/terms' component={Terms} />
-
-    //             {admintoShow}
-    //             <Redirect to='/' />
-    //         </Switch>
-    //     )
-    // }
+   
 
     return (
         <div className='rootApp'>
