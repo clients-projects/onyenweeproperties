f03c58613a7620d5767bb7b7a9822bcc6fbe162f
Igboanugwo Collins <munisco12@gmail.com>
2021-04-18 15:19:57 +0100

    Working on the card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,8 +1,19 @@
+import RealEstate1 from '../assets/realEstate/RealEstate1.jpg'
+
+
 const Card = () => {
-    return <div className='card'>
-        
-        This is the card
+    return (
+        <div className='card'>
+            <div className='card__img'>
+                <img
+                    src={RealEstate1}
+                    alt='realEstate'
+                    className='realEstate_img'
+                />
+            </div>
+            This is the card
         </div>
+    )
 }
 
 export default Card
