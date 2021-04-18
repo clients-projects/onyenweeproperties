602ff8eed391fa435bb9fe67f434164606b3b627
Igboanugwo Collins <munisco12@gmail.com>
2021-04-18 18:51:50 +0100

    Working on the card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -5,11 +5,10 @@ const Card = () => {
     return (
         <div className='card'>
             <div className='card__img'>
-                <img
-                    src={RealEstate1}
-                    alt='realEstate'
-                    className='realEstate_img'
-                />
+                <img src={RealEstate1} alt='card' className='card__img--item' />
+
+                <h1 className='card__img--name'>Multi family house</h1>
+                <h1 className='card__img--price'>N423,232</h1>
             </div>
             This is the card
         </div>
