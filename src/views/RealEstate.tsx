86b00f25ec26eb0d7b66892435a4c187f75cc432
Igboanugwo Collins <munisco12@gmail.com>
2021-04-18 07:51:10 +0100

    Added real estate assets

--- src/views/RealEstate.tsx
+++ src/views/RealEstate.tsx
@@ -1,6 +1,10 @@
+import RealEstate1 from '../assets/realEstate/RealEstate1.jpg'
+
 const RealEstate = () => {
     return (
         <div className='realEstate'>
+
+            <img src={RealEstate1} alt='realEstate'/>
             RealEstate page
         </div>
     )
