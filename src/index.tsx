f5655a8a87b60102e25d2764f9d33a6f10196c5b
Igboanugwo Collins <munisco12@gmail.com>
2021-04-18 07:24:03 +0100

    Removed redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,18 @@
 import './sass/main.scss'
 import ReactDom from 'react-dom'
 
-import {createStore, applyMiddleware} from 'redux'
-import {Provider} from 'react-redux'
-import thunk from 'redux-thunk'
-import { reducers } from './store/reducers/reducer'
+
 import App from './Containers/App'
 import { LastLocationProvider } from 'react-router-last-location'
 import { BrowserRouter } from 'react-router-dom'
 
-const store = createStore(reducers, applyMiddleware(thunk))
 
 const app = (    
-    <Provider store={store}>
         <BrowserRouter>
         <LastLocationProvider>
              <App/>
         </LastLocationProvider>
         </BrowserRouter>
-    </Provider>
 )
 
 ReactDom.render(app, document.querySelector('#root'))
