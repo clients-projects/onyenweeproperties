cff86d2fd24ef3433e68eabac5a6b52f39c599b5
Igboanugwo Collins <munisco12@gmail.com>
2021-04-19 07:07:03 +0100

    Working on stories and removed realtors as the client required

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,14 +1,12 @@
 import Features from '../components/Features'
 import Hero from '../components/Hero'
 import Homes from '../components/Homes'
-import Realtors from '../components/Realtors'
 import Story from '../components/Story'
 
 const Home = () => {
     return (
         <>
             <Hero />
-            <Realtors />
             <Features />
             <Story />
             <Homes />
