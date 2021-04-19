46f743dbb421ea277b2e8fe9fd939181089a070d
Igboanugwo Collins <munisco12@gmail.com>
2021-04-19 17:49:07 +0100

    Working on real estate view

--- src/views/RealEstate.tsx
+++ src/views/RealEstate.tsx
@@ -17,8 +17,8 @@ const RealEstate = () => {
         <>
             <header className='hero' style={bgImg}>
                 <div className='hero__1'>
-                    <h3 className='heading-3'>Your own home:</h3>
-                    <h1 className='heading-1'>Exquisite Homes Just For You</h1>
+                    <h3 className='heading-3'>Real Estate:</h3>
+                    <h1 className='heading-1'>Our Real Estate Services</h1>
                 </div>
                 <button className='btn hero__btn'>Contact Us Today</button>
             </header>
