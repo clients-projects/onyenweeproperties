8816d3401aca021160c9bedef42d677087ea431a
Igboanugwo Collins <munisco12@gmail.com>
2021-04-19 16:10:05 +0100

    Working on real estate view

--- src/views/RealEstate.tsx
+++ src/views/RealEstate.tsx
@@ -28,7 +28,14 @@ const RealEstate = () => {
         //     </div>
         // </div>
         <>
-            <Hero />
+            <header className='hero'>
+                <div className='hero__1'>
+                    <h3 className='heading-3'>Your own home:</h3>
+                    <h1 className='heading-1'>Exquisite Homes Just For You</h1>
+                </div>
+                <button className='btn hero__btn'>Contact Us Today</button>
+
+            </header>
             <Features />
         </>
     )
