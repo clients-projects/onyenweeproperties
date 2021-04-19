a15dff2dfda5e17f66ce9ea750033789220e3cfe
Igboanugwo Collins <munisco12@gmail.com>
2021-04-19 14:01:33 +0100

    Working on homes component

--- src/components/Homes.tsx
+++ src/components/Homes.tsx
@@ -10,157 +10,126 @@ const Homes = () => {
     return (
         <>
             <h1>Services </h1>
-        <section className="homes">
-        <div className="home">
-            <img src={House1} alt="House 1" className="home__img"/>
-            <svg className="home__like">
-                 {/* <use xlinkHref={SpriteHeartFull}></use>  */}e
-            </svg>
-            <h5 className="home__name">Beautiful Family Home</h5>
-            <div className="home__location">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-map-pin"></use> */}
-                </svg>
-                <p>USA</p>
-            </div>
+            <section className='homes'>
+                <div className='home'>
+                    <img src={House1} alt='House 1' className='home__img' />
+                    <svg className='home__like'></svg>
+                    <h5 className='home__name'>Beautiful Family Home</h5>
+                    <div className='home__location'>
+                        <p>USA</p>
+                    </div>
 
-            <div className="home__rooms">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-profile-male"></use> */}
-                </svg>
-                <p>5 rooms</p>
-            </div>
+                    <div className='home__rooms'>
+                        <p>5 rooms</p>
+                    </div>
 
-            <div className="home__area">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-expand"></use> */}
-                </svg>
-                <p>325 m<sup>2</sup></p>
-            </div>
+                    <div className='home__area'>
+                        <svg></svg>
+                        <p>
+                            325 m<sup>2</sup>
+                        </p>
+                    </div>
 
-            <div className="home__price">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-key"></use> */}
-                </svg>
-                <p>$1,200,000</p>
-            </div>
-            <button className="btn home__btn">View Property</button>
-        </div>
+                    <div className='home__price'>
+                        <svg></svg>
+                        <p>₦1,200,000</p>
+                    </div>
+                    <button className='btn home__btn'>View Property</button>
+                </div>
 
-        <div className="home">
-            <img src={House2} alt="House 1" className="home__img"/>
-            <svg className="home__like">
-                {/* <use xlinkHref="img/sprite.svg#icon-heart-full"></use> */}
-            </svg>
-            <h5 className="home__name">Modern Glass Villa</h5>
-            <div className="home__location">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-map-pin"></use> */}
-                </svg>
-                <p>Canada</p>
-            </div>
+                <div className='home'>
+                    <img src={House2} alt='House 1' className='home__img' />
 
-            <div className="home__rooms">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-profile-male"></use> */}
-                </svg>
-                <p>6 rooms</p>
-            </div>
+                    <h5 className='home__name'>Modern Glass Villa</h5>
+                    <div className='home__location'>
+                        <p>Canada</p>
+                    </div>
 
-            <div className="home__area">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-expand"></use> */}
-                </svg>
-                <p>450 m<sup>2</sup></p>
-            </div>
+                    <div className='home__rooms'>
+                        <p>6 rooms</p>
+                    </div>
 
-            <div className="home__price">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-key"></use> */}
-                </svg>
-                <p>$2,750,000</p>
-            </div>
-            <button className="btn home__btn">View Property</button>
-        </div>
+                    <div className='home__area'>
+                        <p>
+                            450 m<sup>2</sup>
+                        </p>
+                    </div>
 
-        <div className="home">
-            <img src={House3} alt="House 3" className="home__img"/>
-            <svg className="home__like">
-                {/* <use xlinkHref="img/sprite.svg#icon-heart-full"></use> */}
-            </svg>
-            <h5 className="home__name">Cozy Country House</h5>
-            <div className="home__location">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-map-pin"></use> */}
-                </svg>
-                <p>UK</p>
-            </div>
+                    <div className='home__price'>
+                        <p>₦2,750,000</p>
+                    </div>
+                    <button className='btn home__btn'>View Property</button>
+                </div>
 
-            <div className="home__rooms">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-profile-male"></use> */}
-                </svg>
-                <p>4 rooms</p>
-            </div>
+                <div className='home'>
+                    <img src={House3} alt='House 3' className='home__img' />
+                    <svg className='home__like'>
+                        {/* <use xlinkHref="img/sprite.svg#icon-heart-full"></use> */}
+                    </svg>
+                    <h5 className='home__name'>Cozy Country House</h5>
+                    <div className='home__location'>
+                        <svg>
+                            {/* <use xlinkHref="img/sprite.svg#icon-map-pin"></use> */}
+                        </svg>
+                        <p>UK</p>
+                    </div>
 
-            <div className="home__area">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-expand"></use> */}
-                </svg>
-                <p>250 m<sup>2</sup></p>
-            </div>
+                    <div className='home__rooms'>
+                        <svg>
+                            {/* <use xlinkHref="img/sprite.svg#icon-profile-male"></use> */}
+                        </svg>
+                        <p>4 rooms</p>
+                    </div>
 
-            <div className="home__price">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-key"></use> */}
-                </svg>
-                <p>$850,000</p>
-            </div>
-            <button className="btn home__btn">View Property</button>
-        </div>
+                    <div className='home__area'>
+                        <svg>
+                            {/* <use xlinkHref="img/sprite.svg#icon-expand"></use> */}
+                        </svg>
+                        <p>
+                            250 m<sup>2</sup>
+                        </p>
+                    </div>
 
-        <div className="home">
-            <img src={House4} alt="House 1" className="home__img"/>
-            <svg className="home__like">
-                {/* <use xlinkHref="img/sprite.svg#icon-heart-full"></use> */}
-            </svg>
-            <h5 className="home__name">Large Country Villa</h5>
-            <div className="home__location">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-map-pin"></use> */}
-                </svg>
-                <p>Portugal</p>
-            </div>
+                    <div className='home__price'>
+                        <svg>
+                            {/* <use xlinkHref="img/sprite.svg#icon-key"></use> */}
+                        </svg>
+                        <p>₦850,000</p>
+                    </div>
+                    <button className='btn home__btn'>View Property</button>
+                </div>
 
-            <div className="home__rooms">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-profile-male"></use> */}
-                </svg>
-                <p>6 rooms</p>
-            </div>
+                <div className='home'>
+                    <img src={House4} alt='House 1' className='home__img' />
+                  g>
+                    <h5 className='home__name'>Large Country Villa</h5>
+                    <div className='home__location'>
 
-            <div className="home__area">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-expand"></use> */}
-                </svg>
-                <p>480 m<sup>2</sup></p>
-            </div>
+                        <p>Maitama</p>
+                    </div>
 
-            <div className="home__price">
-                <svg>
-                    {/* <use xlinkHref="img/sprite.svg#icon-key"></use> */}
-                </svg>
-                <p>$1,950,000</p>
-            </div>
-            <button className="btn home__btn">View Property</button>
-        </div>
+                    <div className='home__rooms'>
+                     
+                        <p>6 rooms</p>
+                    </div>
 
-        
+                    <div className='home__area'>
+                     
+                        <p>
+                            480 m<sup>2</sup>
+                        </p>
+                    </div>
 
-       
-
-    </section>
-    </>
+                    <div className='home__price'>
+                        <svg>
+                            {/* <use xlinkHref="img/sprite.svg#icon-key"></use> */}
+                        </svg>
+                        <p>₦1,950,000</p>
+                    </div>
+                    <button className='btn home__btn'>View Property</button>
+                </div>
+            </section>
+        </>
     )
 }
 
