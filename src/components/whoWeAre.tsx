fa481dae4519af7c6313f44542272af624864243
Igboanugwo Collins <munisco12@gmail.com>
2021-04-19 07:35:42 +0100

    Working on whoWeare

--- src/components/whoWeAre.tsx
+++ src/components/whoWeAre.tsx
@@ -1,5 +1,23 @@
 const WhoWeAre = () => {
-    return <div> Hello world </div>
+    return (
+        <div>
+            <h2 className='heading-2 heading-2--dark mb-md'>Vision</h2>
+            <p className='story__text'>
+                Onyenwee Properties is a Real Estate and property Management
+                Company located in Awka, Nigeria. We have our operations within
+                the South-Eastern part of Nigeria. We are a team of young and
+                vibrant individuals driven with the goal to provide fantastic
+                realty and property management services and satisfaction to our
+                esteemed customers. We are poised to give best in class realty
+                services, property management, construction and interior
+                services. With solid partnerships built over the years, there is
+                no doubt that we are able to help you succeed in buying or
+                selling properties any where around the south eastern part pf
+                Nigeria!
+            </p>
+     
+        </div>
+    )
 }
 
 export default WhoWeAre
