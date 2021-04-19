fd4b8adbd26768354e72f0875ff883fbe102c233
Igboanugwo Collins <munisco12@gmail.com>
2021-04-19 17:54:32 +0100

    Working on real estate view

--- src/views/PropertyManangement.tsx
+++ src/views/PropertyManangement.tsx
@@ -12,8 +12,8 @@ const PropertyManagement = () => {
         <>
             <header className='hero' style={bgImg}>
                 <div className='hero__1'>
-                    <h3 className='heading-3'>Your own home:</h3>
-                    <h1 className='heading-1'>Exquisite Homes Just For You</h1>
+                    <h3 className='heading-3'>Property Management:</h3>
+                    <h1 className='heading-1'>Manage Your Properties with Us</h1>
                 </div>
                 <button className='btn hero__btn'>Contact Us Today</button>
             </header>
