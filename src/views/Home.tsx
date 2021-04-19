54c4796d0a537ee2019edfdf6a81f34c40d1f142
Igboanugwo Collins <munisco12@gmail.com>
2021-04-19 07:32:54 +0100

    Working on stories

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,11 +2,13 @@ import Features from '../components/Features'
 import Hero from '../components/Hero'
 import Homes from '../components/Homes'
 import Story from '../components/Story'
+import WhoWeAre from '../components/whoWeAre'
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <WhoWeAre/>
             <Features />
             <Story />
             <Homes />
