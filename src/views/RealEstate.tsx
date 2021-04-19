4575f3dd223fe6f62f04c75d0fe36260facdc229
Igboanugwo Collins <munisco12@gmail.com>
2021-04-19 16:06:36 +0100

    Working on homes component

--- src/views/RealEstate.tsx
+++ src/views/RealEstate.tsx
@@ -1,26 +1,40 @@
+import React from 'react'
 import RealEstate1 from '../assets/realEstate/RealEstate1.jpg'
 import Card from '../components/card'
 
+import Features from '../components/Features'
+import Hero from '../components/Hero'
+import Homes from '../components/Homes'
+import Story from '../components/Story'
+import WhoWeAre from '../components/whoWeAre'
+
 const RealEstate = () => {
     return (
-        <div className='realEstate'>
-            <img
-                src={RealEstate1}
-                alt='realEstate'
-                className='realEstate_img'
-            />
-            <div className='realEstate__heading'>
-                <h1 className='heading-1' style={{color: 'black'}}>Real Estate</h1>
-                <p>Our exquisite homes</p>
-            </div>
+        // <div className='realEstate'>
+        //     <img
+        //         src={RealEstate1}
+        //         alt='realEstate'
+        //         className='realEstate_img'
+        //     />
+        //     <div className='realEstate__heading'>
+        //         <h1 className='heading-1' style={{color: 'black'}}>Real Estate</h1>
+        //         <p>Our exquisite homes</p>
+        //     </div>
 
-            <div className='card'>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-            </div>
-        </div>
+        //     <div className='card'>
+        //         <Card/>
+        //         <Card/>
+        //         <Card/>
+        //         <Card/>
+        //     </div>
+        // </div>
+        <>
+            <Hero />
+            <WhoWeAre />
+            <Features />
+            <Story />
+            <Homes />
+        </>
     )
 }
 
