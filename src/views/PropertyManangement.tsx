250680df53d05fe52536a38124f4083a4b5f5433
Igboanugwo Collins <munisco12@gmail.com>
2021-04-19 18:34:24 +0100

    Working on real estate view

--- src/views/PropertyManangement.tsx
+++ src/views/PropertyManangement.tsx
@@ -1,16 +1,21 @@
 import Features from '../components/Features'
 import Hero from '../components/Hero'
-import Homes from '../components/Homes'
-import Story from '../components/Story'
+
 import WhoWeAre from '../components/whoWeAre'
 
 const Home = () => {
     return (
         <>
-            <Hero />
+            <header className='hero'>
+                <div className='hero__1'>
+                    <h3 className='heading-3'>Your own home:</h3>
+                    <h1 className='heading-1'>Exquisite Homes Just For You</h1>
+                </div>
+                <button className='btn hero__btn'>Contact Us Today</button>
+                
+            </header>{' '}
             <WhoWeAre />
             <Features />
-         
         </>
     )
 }
