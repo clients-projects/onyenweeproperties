783f28ca3162ac1960cbf990634046adddbf4669
Igboanugwo Collins <munisco12@gmail.com>
2021-04-19 14:27:17 +0100

    Working on homes component

--- src/components/Homes.tsx
+++ src/components/Homes.tsx
@@ -8,33 +8,33 @@ import House4 from '../sass/img/house-4.jpeg'
 const Homes = () => {
     return (
         <>
-            <h1 className='heading-1 homes__heading'>Services </h1>
-            <section className='homes'>
+            <section className='homes__box'>
+            <h1 className='heading-1 heading-2--dark homes__heading'>
+                Services{' '}
+            </h1>
+            <div className='homes'>
                 <div className='home'>
                     <img src={House1} alt='House 1' className='home__img' />
                     <h5 className='home__name'>Real Estate</h5>
-                  
                 </div>
 
                 <div className='home'>
                     <img src={House2} alt='House 1' className='home__img' />
 
                     <h5 className='home__name'>Property Management</h5>
-                   
                 </div>
 
                 <div className='home'>
                     <img src={House3} alt='House 3' className='home__img' />
 
                     <h5 className='home__name'>Construction</h5>
-                  
                 </div>
 
                 <div className='home'>
                     <img src={House4} alt='House 1' className='home__img' />
 
                     <h5 className='home__name'>Interior</h5>
-
+                </div>
                 </div>
             </section>
         </>
