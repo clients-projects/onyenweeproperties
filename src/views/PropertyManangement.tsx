4ace81d400a62718f35a2a6dcbee29d803aa86b7
Igboanugwo Collins <munisco12@gmail.com>
2021-04-19 18:31:26 +0100

    Working on real estate view

--- src/views/PropertyManangement.tsx
+++ src/views/PropertyManangement.tsx
@@ -1,5 +1,19 @@
-const PropertyManagement = () => {
-    return <div>Property Management page</div>
+import Features from '../components/Features'
+import Hero from '../components/Hero'
+import Homes from '../components/Homes'
+import Story from '../components/Story'
+import WhoWeAre from '../components/whoWeAre'
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <WhoWeAre />
+            <Features />
+            <Story />
+            <Homes />
+        </>
+    )
 }
 
-export default PropertyManagement
+export default Home
