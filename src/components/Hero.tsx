d2410242f2e3a182f0b74e086d404540e7089b55
Igboanugwo Collins <munisco12@gmail.com>
2021-05-18 09:42:38 +0100

    working on hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,27 +10,7 @@ const Hero = () => {
                 <h1 className='heading-1'>Exquisite Homes Just For You</h1>
             </div>
             <Link className='btn hero__btn' to='/contact-us'>Contact Us Today</Link>
-            <div>
-                <div className='hero__followon-text'>Follow us on</div>
-                <div className='hero__followon-logos'>
-                    <Link to='/' className='hero__followon-link'>
-                        {' '}
-                        <FaFacebook />
-                    </Link>
-                    <Link to='/' className='hero__followon-link'>
-                        {' '}
-                        <FaInstagramSquare />
-                    </Link>
-                    <Link to='/' className='hero__followon-link'>
-                        {' '}
-                        <AiFillTwitterCircle />
-                    </Link>
-                    <Link to='/' className='hero__followon-link'>
-                        {' '}
-                        <AiFillLinkedin />
-                    </Link>
-                </div>
-            </div>
+           
         </header>
     )
 }
