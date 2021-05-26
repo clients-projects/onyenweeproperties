dfab1a0503f451e8a2c62e3e5072610cdd342e03
Igboanugwo Collins <munisco12@gmail.com>
2021-05-26 11:19:34 +0100

    working on hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,18 +7,23 @@ import HeroCouple from '../assets/realEstate/heroCouple.png'
 const Hero = () => {
     return (
         <header className='hero'>
-            <div>
-
             <div className='hero__1'>
                 <h3 className='heading-3'>Your own home:</h3>
-                <h1 className='heading-1'>Exquisite Homes Just For You</h1>
+                <h1 className='heading-1 hero__title'>
+                    Exquisite Homes Just For You
+                </h1>
+                <Link className='btn hero__btn' to='/contact-us'>
+                    Contact Us Today
+                </Link>
             </div>
-            <Link className='btn hero__btn' to='/contact-us'>Contact Us Today</Link>
+            <div className='hero__img'>
+                <img
+                    src={HeroCouple}
+                    width='100%'
+                    height='100%'
+                    alt='heroCouple'
+                />
             </div>
-            <div>
-                <img src={HeroCouple} width='100%' height='100%' alt='heroCouple'/>
-            </div>
-           
         </header>
     )
 }
