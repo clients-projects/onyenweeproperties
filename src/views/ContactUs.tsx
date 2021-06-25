4e3c88f1f4ce3ef8c1f29f18781058d490f3f54f
Igboanugwo Collins <munisco12@gmail.com>
2021-06-25 07:05:15 +0100

    Editing layout

--- src/views/ContactUs.tsx
+++ src/views/ContactUs.tsx
@@ -36,7 +36,7 @@ const Home = () => {
     }
     
     const bgImg = {
-        backgroundImage: `linear-gradient(to top, rgba(16, 29, 44, 0.8), rgba(16, 29, 44, 0.4)), url(${RealEstate1})`,
+        backgroundImage: `linear-gradient(to bottom, rgb(191, 191, 191) 20%, rgba(16, 29, 44, 0.4)), url(${RealEstate1})`,
     }
 
     const templateParams = {
