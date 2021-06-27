58bbb2df5b4f6ad60b7d54d5426d86f093b5517f
Igboanugwo Collins <munisco12@gmail.com>
2021-06-27 22:33:14 +0100

    working on input validation

--- src/components/estateServices.tsx
+++ src/components/estateServices.tsx
@@ -5,66 +5,69 @@ import homeRepair from '../assets/homeRepair.png'
 
 const EstateServices = () => {
     return (
-        // <div className='whoWeAre'>
-        //     <h2 className='heading-1 heading-2--dark whoWeAre__title'>
-        //         Welcome to Onyenwee Properties
-        //     </h2>
-        //     <p className='whoWeAre__text'>
-        //         Onyenwee properties NIG LTD ( Mine ) is a liability company
-        //         licensed under the Nigeria corporate affairs commission to deal
-        //         on business of real estate investment. We have our peculiarity
-        //         on developed properties and agency services. We are a team of
-        //         professionals with young vibrant and experienced sales men and
-        //         women, property managers, valuers, architects, Surveyors,
-        //         financial analysts and civil engineers. With over 30years
-        //         experience and dominant in the real estate industry in the south
-        //         Eastern region of the country we have our head office at Rufia
-        //         Garuba Real Estate off Aroma junction Awka State Capital
-        //         Anambara state. Our branch network extends beyond the region
-        //         covering most parts of the country.
-        //     </p>
-        // </div>
-
-        <div className='estateServices'>
-            <div className='estateServices__item'>
-                <img src={buy} alt='' />
-                <h2>I want to Buy</h2>
-                <hr />
-                <p>
-                    Onyenwee Properties have the knowledge and experience to
-                    help you discover the perfect property
+        <>
+            <div className='whoWeAre'>
+                <h2 className='heading-1 heading-2--dark whoWeAre__title'>
+                    Welcome to Onyenwee Properties
+                </h2>
+                <p className='whoWeAre__text'>
+                    Onyenwee properties NIG LTD ( Mine ) is a liability company
+                    licensed under the Nigeria corporate affairs commission to
+                    deal on business of real estate investment. We have our
+                    peculiarity on developed properties and agency services. We
+                    are a team of professionals with young vibrant and
+                    experienced sales men and women, property managers, valuers,
+                    architects, Surveyors, financial analysts and civil
+                    engineers. With over 30years experience and dominant in the
+                    real estate industry in the south Eastern region of the
+                    country we have our head office at Rufia Garuba Real Estate
+                    off Aroma junction Awka State Capital Anambara state. Our
+                    branch network extends beyond the region covering most parts
+                    of the country.
                 </p>
             </div>
-            <div className='estateServices__item'>
-                <img src={rent} alt='' />
-                <h2>I want to Rent</h2>
-                <hr />
-                <p>
-                    We take the time to understand your rental requirements and
-                    work with you to find the property that suits your needs.
-                </p>
+            <div className='estateServices'>
+                <div className='estateServices__item'>
+                    <img src={buy} alt='' />
+                    <h2>I want to Buy</h2>
+                    <hr />
+                    <p>
+                        Onyenwee Properties have the knowledge and experience to
+                        help you discover the perfect property
+                    </p>
+                </div>
+                <div className='estateServices__item'>
+                    <img src={rent} alt='' />
+                    <h2>I want to Rent</h2>
+                    <hr />
+                    <p>
+                        We take the time to understand your rental requirements
+                        and work with you to find the property that suits your
+                        needs.
+                    </p>
+                </div>
+                <div className='estateServices__item'>
+                    <img src={selling} alt='' />
+                    <h2>I want to Sell</h2>
+                    <hr />
+                    <p>
+                        Our team apply their years of experience to help you
+                        achieve the best possible return for your property
+                        within the shortest timeframe.
+                    </p>
+                </div>
+                <div className='estateServices__item'>
+                    <img src={homeRepair} alt='' />
+                    <h2>Property Management</h2>
+                    <hr />
+                    <p>
+                        We ensure peace of mind by securing high quality tenants
+                        and the highest possible rent alongside diligent routine
+                        inspections.
+                    </p>
+                </div>
             </div>
-            <div className='estateServices__item'>
-                <img src={selling} alt='' />
-                <h2>I want to Sell</h2>
-                <hr />
-                <p>
-                    Our team apply their years of experience to help you achieve
-                    the best possible return for your property within the
-                    shortest timeframe.
-                </p>
-            </div>
-            <div className='estateServices__item'>
-                <img src={homeRepair} alt='' />
-                <h2>Property Management</h2>
-                <hr />
-                <p>
-                    We ensure peace of mind by securing high quality tenants and
-                    the highest possible rent alongside diligent routine
-                    inspections.
-                </p>
-            </div>
-        </div>
+        </>
     )
 }
 
