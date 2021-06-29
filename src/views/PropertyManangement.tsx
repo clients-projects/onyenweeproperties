6c9020a3ceae533010af65579aac01cd7fe9c038
Igboanugwo Collins <munisco12@gmail.com>
2021-06-29 08:58:33 +0100

    Added another course

--- src/views/PropertyManangement.tsx
+++ src/views/PropertyManangement.tsx
@@ -1,4 +1,4 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import RealEstate1 from '../assets/realEstate/RealEstate5.jpg'
 
@@ -6,11 +6,10 @@ import { ImEnter, ImExit } from 'react-icons/im'
 import { AiFillHourglass } from 'react-icons/ai'
 
 const Home = () => {
-      const bgImg = {
-          backgroundImage: `linear-gradient(to bottom, rgb(191, 191, 191) 20%, rgba(16, 29, 44, 0.98)), url(${RealEstate1})`,
-      }
+    const bgImg = {
+        backgroundImage: `linear-gradient(to bottom, rgb(191, 191, 191) 20%, rgba(16, 29, 44, 0.98)), url(${RealEstate1})`,
+    }
 
-     
     return (
         <>
             <header className='heropages' style={bgImg}>
@@ -18,7 +17,6 @@ const Home = () => {
                     <h3 className='heading-3'>Property Management:</h3>
                     <h1 className='heading-1'>Lets Manage Your Properties</h1>
                 </div>
-             
             </header>{' '}
             <div className='whoWeAre'>
                 <h2 className='heading-1 heading-2--dark whoWeAre__title'>
@@ -78,6 +76,77 @@ const Home = () => {
                     </p>
                 </div>
             </section>{' '}
+            <div className='whoWeAre'>
+                <h2 className='heading-1 heading-2--dark whoWeAre__title'>
+                    Our Services
+                </h2>
+                <p className='whoWeAre__text'>
+                    <strong>Property Management:</strong> We guarantee you a
+                    peace of mind by securing a high quality tenants quickest
+                    return on property investment diligent routine inspection
+                    and maintenance. We are passionate when it comes to building
+                    your inflow, our commission and the property users easy
+                    access and testimonies contact us I want to sell: Selling of
+                    landed properties could be a challenging experience for a
+                    property owner this is why you need us At Onyenwee
+                    properties NIG LTD (mine), we understand the emotional
+                    attachment of a property owner to a property and the hard
+                    times one goes through while laying off a property for
+                    delimer or for what is worth. We Identify the following
+                    factors influence how a property owner sells his/her
+                    property: 1. Emotions : An individual tags a property to a
+                    problem this makes the owner vulnerable as the potential
+                    purchaser would psychologically capitalize on this weakness
+                    to bid down their price 2. Marketing Avenue : Specific goods
+                    and services are better appreciated valued and sort for at
+                    the right market one of the biggest challenges for an
+                    individuals aiming to sell a property at a full value is not
+                    knowing the right market thereby left with no option than
+                    laying off the property at a random market on an
+                    intimidation low price 3. Price bidding mechanism: A
+                    property purchaser will always bid low while dealing with an
+                    individuals believing you would lay off the property once
+                    his bid matches your emotions Why We Should Help You Sell We
+                    sidelines emotions and your property also got appraisal off
+                    our good will. Onyenwee properties NIG LTD is a big name in
+                    the property industry within the south Eastern part of the
+                    country. This is what we've been known for over 30years
+                    still waxing stronger. Marketing Avenue: Onyenwee properties
+                    for over 30 years of dominant in property industry
+                    southeastern Nigeria is fast becoming a house hold name and
+                    number one choice for real estate developers within the
+                    country. We have our marketing agents on and around every
+                    Conner within the region. We also make use of social and
+                    conversational media ie newspaper's magazines, radio
+                    stations as a medium of reach out to the public Price
+                    bidding mechanism: Property pricing can never be a talent
+                    rather a combination of the sellers position, long time
+                    experience and also the understanding of the present and
+                    future economic value of a property. Our team of sales
+                    experts are there for you contact us for cool price, cool
+                    commission cool ownership and sense of accomplishment via
+                    the purchaser and everything one goes home happy I wan to
+                    buy Buying a landed property could the a challenging
+                    experience for a prospective purchaser the following are
+                    factors to consider and why you need our assistance in this
+                    regards: The authenticity of the property Valid documents
+                    and property value For more enquiry contact us I want to
+                    rent There are a lot of challenges when it comes to renting
+                    a conducive apartment but with Onyenwee properties managers
+                    you could go way RENT MADE EASY For enquiry on both
+                    landlords and tenants guild book, contact us Space for
+                    available properties for sell and for rent. Real Estate :
+                    Land survey and lodgments, selling of lands, town planning
+                    Construction: we have an team of certified skills architects
+                    and structural engineer young experience and innovative mind
+                    that knows what taste is all about when it's comes to for
+                    private, commercial and public uses. Interior design and
+                    furnishing equipment: Contact us for your office furniture
+                    and interior we are into sales and purchase of furniture,
+                    toiletries, electronics appliances and house hold equipment
+                    whole sale supply hotels and public centers.
+                </p>
+            </div>{' '}
         </>
     )
 }
