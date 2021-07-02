5614cc3e7397296d9a7fa4ede5dbaa414709983d
Igboanugwo Collins <munisco12@gmail.com>
2021-07-02 18:09:11 +0100

    updated page

--- src/views/ContactUs.tsx
+++ src/views/ContactUs.tsx
@@ -9,7 +9,6 @@ import { FaFacebook, FaInstagramSquare } from 'react-icons/fa'
 import { AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 
-
 const Home = () => {
     const [email, setEmail] = useState('')
     const [message, setMessage] = useState('')
@@ -17,7 +16,7 @@ const Home = () => {
     const [handleSummit, setHandleSummit] = useState(false)
     const [error, setError] = useState(false)
     const [displayMessage, setDisplayMessage] = useState('')
-    
+
     const inputHandler = (input: any) => {
         const inputName = input.target.name
         const inputValue = input.target.value
@@ -34,22 +33,22 @@ const Home = () => {
             setMessage(inputValue)
         }
     }
-    
+
     const bgImg = {
         backgroundImage: `linear-gradient(to bottom, rgb(191, 191, 191) 20%, rgba(16, 29, 44, 0.8)), url(${RealEstate1})`,
     }
 
     const templateParams = {
-      user_name: name,
-      user_email:email,
-      message
-    };
-    
+        user_name: name,
+        user_email: email,
+        message,
+    }
+
     const handleForm = (e: any) => {
         e.preventDefault()
-        
+
         setHandleSummit(true)
-        
+
         if (email === '' || message === '' || name === '') {
             setError(true)
             console.log('not sent')
@@ -59,9 +58,14 @@ const Home = () => {
             setDisplayMessage('...Sending')
 
             emailjs
-            .send('service_ug3kztv', 'template_1fpejxx', templateParams, 'user_TyUAlfFTW7kMzOobPhtRV')
-            .then(
-                (result) => {
+                .send(
+                    'service_ug3kztv',
+                    'template_1fpejxx',
+                    templateParams,
+                    'user_TyUAlfFTW7kMzOobPhtRV'
+                )
+                .then(
+                    (result) => {
                         setDisplayMessage('Message sent')
                         console.log(result.text, 'email sent')
                         setEmail('')
@@ -72,7 +76,6 @@ const Home = () => {
                         console.log(error, 'email failed')
                     }
                 )
-
         }
     }
 
@@ -84,68 +87,72 @@ const Home = () => {
                     <h1 className='heading-1'>Always Available For You</h1>
                 </div>
             </header>
-            <div className='contact-box'>
-                <h2 className='heading-1 heading-2--dark contact-box__heading'>
-                    contact form
-                </h2>
-                {handleSummit && (
-                    <p
-                        className={
-                            error
-                                ? 'form__displayError'
-                                : 'form__displayMessage'
-                        }
-                    >
-                        {displayMessage}
-                    </p>
-                )}
-                <form className='contact-form' onSubmit={handleForm}>
-                    <div className='contact-form__item'>
-                        <label htmlFor='name'>Your name:</label>
-                        <input
-                            className='contact-form__input'
-                            onChange={inputHandler}
-                            type='text'
-                            name='user_name'
-                            value={name}
-                        />
+            <div className='features1'>
+                <div className='contact-box'>
+                    <h2 className='heading-1 heading-2--dark contact-box__heading'>
+                        contact form
+                    </h2>
+                    {handleSummit && (
+                        <p
+                            className={
+                                error
+                                    ? 'form__displayError'
+                                    : 'form__displayMessage'
+                            }
+                        >
+                            {displayMessage}
+                        </p>
+                    )}
+                    <form className='contact-form' onSubmit={handleForm}>
+                        <div className='contact-form__item'>
+                            <label htmlFor='name'>Your name:</label>
+                            <input
+                                className='contact-form__input'
+                                onChange={inputHandler}
+                                type='text'
+                                name='user_name'
+                                value={name}
+                            />
+                        </div>
+
+                        <div className='contact-form__item'>
+                            <label htmlFor='mail'>Your e-mail:</label>
+                            <input
+                                className='contact-form__input'
+                                onChange={inputHandler}
+                                type='text'
+                                name='user_email'
+                                value={email}
+                            />
+                        </div>
+
+                        <div className='contact-form__item'>
+                            <label htmlFor=''>Message:</label>
+                            <textarea
+                                className='contact-form__input'
+                                name='message'
+                                onChange={inputHandler}
+                                value={message}
+                                rows={5}
+                            ></textarea>
+                        </div>
+                        <button type='submit' className='contact-form__btn'>
+                            send
+                        </button>
+                    </form>
+
+                    <div className='contact-box__details'>
+                        <FaMapPin />
+                        <p>
+                            Rufai Garuba Street, Real Estate, Awka Anambra state
+                        </p>
+
+                        <AiFillPhone />
+                        <p>08161865277, 08068789462</p>
+
+                        <MdEmail />
+                        <p>onyenweeproperties@gmail.com</p>
                     </div>
-
-                    <div className='contact-form__item'>
-                        <label htmlFor='mail'>Your e-mail:</label>
-                        <input
-                            className='contact-form__input'
-                            onChange={inputHandler}
-                            type='text'
-                            name='user_email'
-                            value={email}
-                        />
-                    </div>
-
-                    <div className='contact-form__item'>
-                        <label htmlFor=''>Message:</label>
-                        <textarea
-                            className='contact-form__input'
-                            name='message'
-                            onChange={inputHandler}
-                            value={message}
-                            rows={5}
-                        ></textarea>
-                    </div>
-                    <button type='submit' className='contact-form__btn'>
-                        send
-                    </button>
-                </form>
-
-                <div className='contact-box__details'>
-                    <FaMapPin />
-                    <p>Rufai Garuba Street, Real Estate, Awka Anambra state</p>
-
-                    <AiFillPhone />
-                    <p>08161865277, 08068789462</p>
-
-                    <MdEmail />
-                    <p>onyenweeproperties@gmail.com</p>
                 </div>
             </div>
         </>
