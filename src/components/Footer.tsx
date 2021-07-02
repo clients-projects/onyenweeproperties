7e4e7487422e35d26187376fccef83394cb26bc0
Igboanugwo Collins <munisco12@gmail.com>
2021-07-02 21:59:41 +0100

    updated page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,38 +5,7 @@ const Footer = () => {
 
     return (
         <footer>
-            {/* <ul className='nav'>
-                <li className='nav__item'>
-                    <a href='#' className='nav__link'>
-                        Find your dream home
-                    </a>
-                </li>
-                <li className='nav__item'>
-                    <a href='#' className='nav__link'>
-                        Request a proposal
-                    </a>
-                </li>
-                <li className='nav__item'>
-                    <a href='#' className='nav__link'>
-                        Download your home planner
-                    </a>
-                </li>
-                <li className='nav__item'>
-                    <a href='#' className='nav__link'>
-                        Contact us
-                    </a>
-                </li>
-                <li className='nav__item'>
-                    <a href='#' className='nav__link'>
-                        Submit your property
-                    </a>
-                </li>
-                <li className='nav__item'>
-                    <a href='#' className='nav__link'>
-                        Come work with us
-                    </a>
-                </li>
-            </ul> */}
+         
             <p className='copyright'>
                 &copy;Copyright {yearDisplay} &mdash; Onyenwee Property
             </p>
