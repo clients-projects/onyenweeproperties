339949542b7d40e171cb7618dd997a9a0e02271e
Igboanugwo Collins <munisco12@gmail.com>
2021-07-02 17:43:06 +0100

    updated page

--- src/views/PropertyManangement.tsx
+++ src/views/PropertyManangement.tsx
@@ -201,6 +201,60 @@ const PropertyManagement = () => {
                  
                 </div>
             </div>{' '} */}
+            <section className='features'>
+                <div className='feature'>
+                    <figure className='feature__icon'>
+                    </figure>
+                    <h4 className='heading-4 heading-4--dark'>Land Survey</h4>
+                    <p className='feature__text'>
+                        We provide superior land survey services; helping you to
+                        locate, describe, monument, and map the boundaries and
+                        corners of a parcel of land.
+                    </p>
+                </div>
+                <div className='feature'>
+                    <figure className='feature__icon'>
+                    </figure>
+                    <h4 className='heading-4 heading-4--dark'>
+                        Property Evaluation
+                    </h4>
+                    <p className='feature__text'>
+                        Estimating the value of your property could be an
+                        arduous task. At Onyenwee Properties, we use both
+                        traditional and advanced methods of property valuation
+                        to give proper valuation of your property. We guarantee
+                        that you get the best value for your property. Try us
+                        today!
+                    </p>
+                </div>
+                <div className='feature'>
+                    <figure className='feature__icon'>
+                    </figure>
+                    <h4 className='heading-4 heading-4--dark'>Town Planning</h4>
+                    <p className='feature__text'>
+                        We work with governments and estate owners in managing
+                        land resources. This involves the control of existing
+                        and new developments, as well as strategy preparation to
+                        ensure manage future requirements. It is a dynamic
+                        process that changes in response to policy, development
+                        proposals and local needs.
+                    </p>
+                </div>
+                <div className='feature'>
+                    <figure className='feature__icon'>
+                    </figure>
+                    <h4 className='heading-4 heading-4--dark'>
+                        Land Sale and Purchase
+                    </h4>
+                    <p className='feature__text'>
+                        We serve as middle men, helping individuals and
+                        corporate bodies purchase or sell land in seamless
+                        manner. We help your source for the land at good prices
+                        and also help the owners negotiate in order to get the
+                        best prices for their lands.
+                    </p>
+                </div>
+            </section>{' '}
         </>
     )
 }
